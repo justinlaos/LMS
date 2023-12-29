e66a1b8a5e12c4be978cc827b597229fc59d17a8
Justin Laos <4jlaos@gmail.com>
2023-12-29 03:22:21 -0700

    set up of author, book, library classes with basic tests

--- lms_cli_app.rb
+++ lms_cli_app.rb
@@ -1,7 +1,40 @@
-#!/usr/bin/env ruby
+require 'json'
 
-class Main
-    def self.talk
-        'Hello, LMS!'
+class Library
+    attr_accessor :books
+    def initialize
+        @books = []
     end
+end
+
+class Author
+    attr_accessor :name
+    def initialize(name)
+        @name = name
+    end
+end
+
+class Book
+    attr_accessor :title, :author, :isbn, :available
+    def initialize(title, author, isbn)
+        @title = title
+        @author = author
+        @isbn = isbn
+        @available = true
+    end
+end
+
+
+# User interaction, Comment out to run tests
+puts "Welcome to the Library Management System"
+
+loop do
+    puts "\nOptions:"
+    puts "1. Display Library Catalog"
+    puts "2. Search for a Book"
+    puts "3. Check Out a Book"
+    puts "4. Return a Book"
+    puts "5. Exit"
+    print "Enter your choice: "
+    choice = gets.chomp.to_i
 end
--- lms_cli_app_test.rb
+++ lms_cli_app_test.rb
@@ -1,9 +1,21 @@
 require 'test/unit'
-require './lms_cli_app.rb'
+require_relative 'lms_cli_app'
 
-class MainTest < Test::Unit::TestCase
-    def test_world
-      assert_equal "Hello, LMS!", Main.talk
+class TestAuthor < Test::Unit::TestCase
+    def test_create_author_with_name
+        author = Author.new("Test Author")
+        assert_equal("Test Author", author.name)
+    end
+end
+
+class TestBook < Test::Unit::TestCase
+    def test_create_book_with_attributes
+        author = Author.new("Test Author")
+        book = Book.new("Test Book", author, "123456789")
+        assert_equal("Test Book", book.title)
+        assert_equal(author, book.author)
+        assert_equal("123456789", book.isbn)
+        assert_equal(true, book.available)
     end
 end
 
